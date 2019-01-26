9ee832333a42c5d28c6c29976e94bf18a51c029e
Heather Kirk <hskirk@gmail.com>
2019-01-26 23:26:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  kimchi = ""
+ new_hash = ""
  if hash.each do |key, value|
-   kimchi = first 
+   new_hash = key
  else 
   hash
 end
