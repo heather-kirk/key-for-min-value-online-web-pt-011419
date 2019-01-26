936b5affaeb18bae9044ddf6091d313e490527f9
Heather Kirk <hskirk@gmail.com>
2019-01-26 23:23:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(best_friend)
+def key_for_min_value(hasj)
   kimchi = ""
  if best_friend.each do |first, last|
    kimchi = first 
