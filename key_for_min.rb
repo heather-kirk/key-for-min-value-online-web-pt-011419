bd72463da25573b08405a4ed59c751374e759919
Heather Kirk <hskirk@gmail.com>
2019-01-26 23:49:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(hash)
   min_value = nil
   hash.each do |key, value|
     if min_value == nil 
-      min_value = value 
+      min_value = key
     end 
     if min_value > key 
-       min_value = value 
+       min_value = key
     end
   end
   min_value
